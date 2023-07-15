6bb95f4288c0135fd42525386b34b7a2a44d8781
YevhenYalovenko <jenyaya0007@gmail.com>
2023-07-16 00:36:39 +0300

    доопрацювання налаштувань

--- app/config.py
+++ app/config.py
@@ -5,4 +5,3 @@ ROOT_DIR: Final[pathlib.Path] = pathlib.Path(__file__).parents[1]
 FILES_INPUT_DIR: Final[pathlib.Path] = ROOT_DIR.joinpath("files_input")
 FILES_OUTPUT_DIR: Final[pathlib.Path] = ROOT_DIR.joinpath("files_output")
 
-LOGS_DIR: Final[pathlib.Path] = ROOT_DIR.joinpath("logs")

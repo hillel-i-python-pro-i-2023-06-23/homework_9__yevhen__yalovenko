b1fff6c9f6a324fc26d08a5694c37681814125f7
YevhenYalovenko <jenyaya0007@gmail.com>
2023-07-23 14:56:19 +0300

    Working on CRUD. C, R, U complete

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, send_file
+from flask import Flask, send_file, Response
 from webargs import fields
 from webargs.flaskparser import use_args
 
 from application.config import FILES_OUTPUT_DIR
+from application.services.create_table import create_table
 from application.services.csv_reader import read_csv, calculate_average_height_weight
+from application.services.db_connection import DBConnection
 from application.services.processing_json import output_json, getting_request
 from application.services.processing_users import format_users, output_user_info
 from application.services.read_file import read_file
@@ -14,6 +16,72 @@ app = Flask(__name__)
 @app.route("/")
 def home_page():
     return "This is homepage"
+
+
+@app.route("/phones/create")
+@use_args({"contact_name": fields.Str(required=True), "phone_value": fields.Int(required=True)}, location="query")
+def phones_create(args):
+    with DBConnection() as connection:
+        with connection:
+            connection.execute(
+                "INSERT INTO phones (contact_name, phone_value) VALUES (:contact_name, :phone_value)",
+                {"contact_name": args["contact_name"], "phone_value": args["phone_value"]},
+            )
+
+    return "Ok"
+
+
+@app.route("/phones/read-all")
+def phones_read_all():
+    with DBConnection() as connection:
+        phones = connection.execute("SELECT * FROM phones;").fetchall()
+    return "<br>".join([f'{phone["user_id"]}: {phone["contact_name"]} - {phone["phone_value"]}' for phone in phones])
+
+
+@app.route("/phones/read/<int:user_id>")
+def phones_read(user_id: int):
+    with DBConnection() as connection:
+        phone = connection.execute(
+            "SELECT * FROM phones WHERE (user_id=:user_id);",
+            {
+                "user_id": user_id,
+            },
+        ).fetchone()
+
+    return f'{phone["user_id"]}: {phone["contact_name"]} - {phone["phone_value"]}'
+
+
+@app.route("/phones/update/<int:user_id>")
+@use_args({"contact_name": fields.Str(), "phone_value": fields.Int()}, location="query")
+def phone_update(args, user_id: int):
+    with DBConnection() as connection:
+        with connection:
+            contact_name = args.get("contact_name")
+            phone_value = args.get("phone_value")
+            if contact_name is None and phone_value is None:
+                return Response(
+                    "Need to provide at least one argument",
+                    status=400,
+                )
+
+            args_for_request = []
+            if contact_name is not None:
+                args_for_request.append("contact_name=:contact_name")
+            if phone_value is not None:
+                args_for_request.append("phone_value=:phone_value")
+
+            args_2 = ", ".join(args_for_request)
+
+            connection.execute(
+                "UPDATE phones " f"SET {args_2} WHERE user_id=:user_id;",
+                {
+                    "user_id": user_id,
+                    "contact_name": contact_name,
+                    "phone_value": phone_value,
+                },
+            )
+
+    return "OK"
 
 
 @app.route("/get-content/")
@@ -54,6 +122,8 @@ def average_h_w():
     read = read_csv(url)
     return calculate_average_height_weight(read)
 
+
+create_table()
 
 if __name__ == "__main__":
     app.run()
